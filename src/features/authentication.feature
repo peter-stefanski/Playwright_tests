96c7a6fec00b9aa6d151a62a0d22b3462058fe36
Piotr Stefanski <peterstefanski3@gmail.com>
2026-08-13 09:25:48 +0200

    Remove all comments from repository

--- src/features/authentication.feature
+++ src/features/authentication.feature
@@ -1,19 +1,7 @@
-@ui
 Feature: Authentication
 
-@login
 Scenario: User sign in with valid credentials
 
 Given the user is on the sign in page
 When the user enters valid credentials
 Then the user should be redirected to the main page
-
-
-
-# @register
-# Scenario: User signs up with valid registration data
-
-# Given the user is on the sign up page
-# When the user enters valid registration data
-# Then the user should be redirected to the sign in page
-# And the user submits the registration form
--- src/features/step.definitions/Authentication.steps.js
+++ src/features/step.definitions/Authentication.steps.js
@@ -12,27 +12,6 @@ When('the user enters valid credentials', async function () {
   await this.authPage.submitLogin();
 });
 
-// When('the user enters valid credentials', async function () {
-//   const [response] = await Promise.all([
-//     this.page.waitForResponse(
-//       (response) => response.url().includes('/') && response.status() === 200,
-//     ),
-
-//     this.authPage.fillLoginForm(user.email, user.password),
-//     this.authPage.submitLogin(),
-//   ]);
-
-//   this.loginResponse = response;
-// });
-
-// Then('the user should be redirected to the main page', async function () {
-//   expect(this.loginResponse.ok()).toBeTruthy();
-
-//   await expect(this.page).toHaveURL('https://automationexercise.com');
-
-//   await expect(this.authPage.loggedUser).toHaveText(user.name);
-// });
-
 Then('the user should be redirected to the main page', async function () {
   await expect(this.page).toHaveURL('https://automationexercise.com/');
   await expect(this.authPage.loggedUser).toHaveText(user.name);
